2ad154b819e1fa5db3a4aaf2bf2abb157a6148dc
Petyo Bynkov <petyob@gmail.com>
2023-03-02 08:09:41 +0200

    Update HTTPLogStatistics.py

--- HTTPLogStatistics.py
+++ HTTPLogStatistics.py
@@ -1,3 +1,6 @@
+# Parses Apache HTTP server access.log file to produce some statistics: 1.Top 10 requested urls sorted by hits. 2.Top 10 visitors by ip sorted by hits. 3.Total hits per month sorted by month. 4. Unique visits (by ip) per month. 5. Top 10 ips barchart per month.
+# Author: Petyo Byankov
+
 # RUN: python3 HTTPLogAnalyzer.py access.log
 import argparse
 parser = argparse.ArgumentParser(description='Produce some statistics from a http log file')
